9dd4f7dc5bdce33db929c4ee9aefa671614962d3
Rhuck1 <bobbyhuck@gmail.com>
2021-08-30 07:36:48 -0700

    abc

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,7 +1,12 @@
-
+import requests
+import boto3
+from botocore.exceptions import NoCredentialsError
 
 
 def url_scraper(page_soup):
+    '''
+    Scrapes City of Phoenix Open Data page and gathers csv links for each month for City Checkbook and returns results in a dictionary
+    '''
     
     # grabs each checkbook heading which is descriptive of month and year
     headings = page_soup.findAll("a", {"class":"heading"})
@@ -28,3 +33,45 @@ def url_scraper(page_soup):
     heading_csv_url_dict = {key:value for key, value in zip(checkbook_headings_list, csv_hrefs_list)}
     
     return heading_csv_url_dict
+
+
+def writer(tup):
+    '''Input: Takes a (key, value) tuple generated from iter() method. Key, Vals represent the date and csv link scraped earlier
+       Output: Downloads csv data and saves to data directory and outupts file path of downloaded csv
+    '''
+    
+    # gets http response from City Checkbook csv link
+    r = requests.get(tup[1])
+    
+    # creates desired filepath for download
+    file_path = 'data/{}.csv'.format(tup[0])
+    
+    # writes csv into filepath
+    open(file_path, 'wb').write(r.content)
+    
+    return file_path
+
+
+def upload_to_aws(local_file, bucket, s3_file):
+    
+    '''
+    Uploads file to AWS S3 bucket
+    '''
+    
+    # aws_access_key_id and aws_secret_access_key are stored in local .aws config file
+    s3 = boto3.client('s3')
+    
+    # 3 parameters necessary for upload: (file to upload to s3, s3 bucket name, 
+    # the name by which we will save the file in s3)
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print('Upload Successful')
+        return True
+    
+    except FileNotFoundError:
+        print('The file was not found')
+        return False
+    
+    except NoCredentialsError:
+        print('Credentials not available')
+        return False
